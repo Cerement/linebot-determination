55014ebdfe4fa8bb48901abc31f8b8ed97986c8e
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-07-22 18:09:13 +0800

    Fix import bug

--- linebot_site/urls.py
+++ linebot_site/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 #from django.contrib import admin
 
-from . import bot
+from .. import bot
 
 urlpatterns = [
     url(r'^callback$', bot.messageHandler),
