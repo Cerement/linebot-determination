1621aa77f47ce7e441082423c5e47d3df7c8e34b
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-07-22 18:24:57 +0800

    Move bot back

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse
 import os
 
 credentials = {
-    'channel_id': str(os.environ.get('Channel_ID')),
-    'channel_secret': str(os.environ.get('Channel_Secret')),
-    'channel_mid': str(os.environ.get('MID')),
+    'channel_id': str(os.environ.get('Channel_ID', 0)),
+    'channel_secret': str(os.environ.get('Channel_Secret', 0)),
+    'channel_mid': str(os.environ.get('MID', 0)),
 }
 
 client = LineBotClient(**credentials)
